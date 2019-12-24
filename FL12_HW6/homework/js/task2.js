c0c6d78fb2c43dc4cee1d3ca9e18247ea839763e
Volodymyr Berestovskyi <vberestovskyy@gmail.com>
2019-12-24 22:13:19 +0200

    fixed float acceptance

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -6,7 +6,9 @@ let c = prompt('Enter length for triangular "C" side', '');
 if (!isFinite(a) || !a || !isFinite(b) || !b || !isFinite(c) || !c) {
   doesExist = false;
   alert('Input values should be ONLY numbers');
-} else if (parseInt(a) <= 0 || parseInt(b) <= 0 || parseInt(c) <= 0) {
+} else if (parseInt(a) <= 0 || !Number.isInteger(parseFloat(a))|| parseInt(b) <= 0 || 
+!Number.isInteger(parseFloat(b)) || parseInt(c) <= 0 || !Number.isInteger(parseFloat(c))) {
+  doesExist = false;
   alert('A triangle must have 3 sides with a positive definite length');
 }
 
